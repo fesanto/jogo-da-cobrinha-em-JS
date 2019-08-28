21a0a5d347fc4da542347753040b59d0c11d8545
fesanto <fgesanto@gmail.com>
2019-08-28 17:27:41 -0300

    joguinho terminado

--- script.js
+++ script.js
@@ -43,11 +43,19 @@ function update(event) {
 }
 
 function iniciarJogo() {
+
     //impedir a cobrinha de sair dos limites do jogo
     if (snake[0].x > 15 * box && direction == 'right') snake[0].x = 0; //se a cobrinha passar de 15, voltará a ter valor 0 e aparecerá do outro lado
     if (snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
     if (snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
     if (snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+
+    for (i = 1; i < snake.length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo);
+            alert('Game Over');
+        }
+    }
 
     criarBG();
     criarSnake();
@@ -62,8 +70,12 @@ function iniciarJogo() {
     if (direction == 'up') snakeY -= box;
     if (direction == 'down') snakeY += box;
 
-    //função pop retira o ultimo elemento do array
-    snake.pop();
+    if (snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     //colocando a cabeça da cobra
     let newHead = {
