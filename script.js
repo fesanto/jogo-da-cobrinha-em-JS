24a0499db7ac9790c8be9265fa44bfda63f2121e
fesanto <fgesanto@gmail.com>
2019-08-28 16:49:51 -0300

    criados os movimentos da cobrinha no plano cartesiano

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = 'right';
 
 //criando o background
 function criarBG() {
@@ -22,5 +23,30 @@ function criarSnake() {
     }
 }
 
-criarBG();
-criarSnake();
+function iniciarJogo() {
+    criarBG();
+    criarSnake();
+
+    let snakeX = snake[0].x; //array na posição 0 de x
+    let snakeY = snake[0].y; //array na posição 0 de y
+
+    //criando coordenadas que serão seguidas. condicionais
+    if (direction == 'right') snakeX += box;
+    if (direction == 'left') snakeX -= box;
+    if (direction == 'up') snakeY -= box;
+    if (direction == 'down') snakeY += box;
+
+    //função pop retira o ultimo elemento do array
+    snake.pop();
+
+    //colocando a cabeça da cobra
+    let newHead = {
+        x: snakeX,
+        y: snakeY,
+    }
+
+    snake.unshift(newHead);
+
+}
+
+let jogo = setInterval(iniciarJogo, 100); //a cada 100 milisegundos ele renova
