b1374c3979f1d0e5a56a3b04dc22934ae744db93
fesanto <fgesanto@gmail.com>
2019-08-28 17:06:36 -0300

    definidas as direçoes para impedir que a cobrinha suma

--- script.js
+++ script.js
@@ -23,7 +23,23 @@ function criarSnake() {
     }
 }
 
+//escutando o botão para determinar onde a cobra irá
+document.addEventListener('keydown', update);
+
+function update(event) {
+    if (event.keyCode == 37 && direction != 'right') direction = 'left';
+    if (event.keyCode == 38 && direction != 'down') direction = 'up';
+    if (event.keyCode == 39 && direction != 'left') direction = 'right';
+    if (event.keyCode == 40 && direction != 'up') direction = 'down';
+}
+
 function iniciarJogo() {
+    //impedir a cobrinha de sair dos limites do jogo
+    if (snake[0].x > 15 * box && direction == 'right') snake[0].x = 0; //se a cobrinha passar de 15, voltará a ter valor 0 e aparecerá do outro lado
+    if (snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
+    if (snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+
     criarBG();
     criarSnake();
 
